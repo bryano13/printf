5bd711ad8c722f5586ccdec89e7d9947de1b1475
jfbm74 <jfbm73@gmail.com>
2020-03-18 02:22:12 +0000

    function that checks printing

---
+++ check_format.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * *get_ptn_fcn - checks if there is a valid format specifier
+ * @format: possible format specifier
+ *
+ * Return: pointer to valid function or NULL
+ */
+int (*get_ptn_fcn(const char *format))(va_list)
+{
+
+	unsigned int i = 0;
+	prtop ops[] = {
+		{"c", print_to_c},
+		{"d", print_to_numbers},
+		{"i", print_to_numbers},
+		{"s", print_to_string},
+		{NULL, NULL}
+	};
+
+	for (i = 0; ops[i].spec != NULL; i++)
+	{
+		if (*(ops[i].spec) == *format)
+		{
+			break;
+		}
+	}
+	return (ops[i].f);
+}
